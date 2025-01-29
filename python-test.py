546886ffa9c9602d0dc505745564b90259f1410c
sakthi <anishaabdulrahiman1999@gmail.com>
2025-01-29 11:58:36 +0530

    to teh repo

---
+++ python-test.py
@@ -0,0 +1,14 @@
+def add_numbers(a, b):
+    return a + b
+
+# Print a message
+print("Hello, World!")
+
+# Add two numbers
+num1 = 5
+num2 = 10
+result = add_numbers(num1, num2)
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {result}")
+
